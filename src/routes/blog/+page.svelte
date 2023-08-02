1b82c333fb4fc3b65f66102a12ad756baf1a5ab8
blackf85 <blackjf85@gmail.com>
2023-08-02 18:52:04 +0700

    Created basic blog page component

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,29 @@
+<div class="text-center p-4">
+	<h2 class="text-2xl font-semibold mb-4">Latest Blog Posts</h2>
+	<ul class="space-y-4">
+		<li>
+			<a href="#" class="block text-xl font-semibold hover:underline"
+				>Exploring Wilderness Survival: Tips for Beginners</a
+			>
+			<p class="text-lg text-gray-700">
+				Learn the basics of wilderness survival, from building shelters to finding food sources.
+			</p>
+		</li>
+		<li>
+			<a href="#" class="block text-xl font-semibold hover:underline"
+				>Gear Guide: Must-Have Tools for Outdoor Adventures</a
+			>
+			<p class="text-lg text-gray-700">
+				Discover the essential gear that will enhance your outdoor experiences.
+			</p>
+		</li>
+		<li>
+			<a href="#" class="block text-xl font-semibold hover:underline"
+				>True Stories: Overcoming Challenges in the Wilderness</a
+			>
+			<p class="text-lg text-gray-700">
+				Read inspiring accounts of individuals who conquered nature's trials.
+			</p>
+		</li>
+	</ul>
+</div>
